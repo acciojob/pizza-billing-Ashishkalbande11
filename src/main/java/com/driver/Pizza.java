c76e2d9f5a6e52bdca819bbb3bb5624b3cbdc262
Ashishkalbande11 <ashishkalbande60>
2024-05-05 14:07:07 +0530

    1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,9 +5,22 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean isExtraCheeseAdded;
+    private  boolean isExtraToppingsAdded;
+    private boolean isExtraPeperAdded;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg){
+            this.price = 300;
+
+        }else{
+            this.price = 400;
+        }
+        this.isExtraPeperAdded = false;
+        this.isExtraCheeseAdded = false;
+        this.isExtraToppingsAdded = false;
         // your code goes here
     }
 
@@ -16,19 +29,57 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
+        if(!isExtraCheeseAdded && isVeg){
+            price += 80;
+        }
+        if(!isExtraCheeseAdded && !isVeg){
+            price += 120;
+        }
+        isExtraCheeseAdded = true;
         // your code goes here
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isExtraToppingsAdded){
+            price += 70;
+
+        }
+        isExtraToppingsAdded = true;
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isExtraPeperAdded){
+            price += 20;
+            isExtraPeperAdded = true;
+        }
     }
 
     public String getBill(){
+
         // your code goes here
-        return this.bill;
+        StringBuilder bill = new StringBuilder();
+        if(isVeg){
+            bill.append("Base Price Of The Pizza: ").append("300").append("\n");
+        }else {
+            bill.append("Base Price Of The Pizza: ").append("400").append("\n");
+        }
+
+        if(isExtraCheeseAdded && isVeg){
+            bill.append("Extra Cheese Added: ").append("80").append("\n");
+        }
+        if(isExtraCheeseAdded && !isVeg){
+            bill.append("Extra Cheese Added: ").append("120").append("\n");
+        }
+        if(isExtraToppingsAdded){
+            bill.append("Extra Toppings Added: ").append("70").append("\n");
+        }
+        if(isExtraPeperAdded){
+            bill.append("Extra Peper Added: ").append("20").append("\n");
+        }
+        bill.append("Total Bill: ").append(getPrice());
+        return bill.toString();
     }
 }
